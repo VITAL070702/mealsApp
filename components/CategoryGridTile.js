51d85543e729ee7c1034c0059f5e0d63f0d49bb4
VITAL070702 <yvitalraquel@gmail.com>
2025-05-22 14:45:54 -0700

    modifique los styles que estaban mal

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,9 +1,9 @@
 import {View,Pressable, Text, StyleSheet, Platform} from "react-native";
 function CategoryGridTile({title, color,onPress}){
     return(
-        <View style={[StyleSheet.gridItem,{backgroundColor:color}]}>
+        <View style={[styles.gridItem,{backgroundColor:color}]}>
             <Pressable android_ripple={{color:'#ccc'}}
-         style={({pressed}) =>[StyleSheet.button, pressed ? style.buttonPressed:null,]}
+         style={({pressed}) =>[styles.button, pressed ? styles.buttonPressed:null,]}
          onPress= {onPress}>
             <View style={styles.innerContainer}>
                 <Text style={styles.title}>
--- components/MealDetails.js
+++ components/MealDetails.js
@@ -1,7 +1,7 @@
 import {View,Text,StyleSheet} from 'react-native'
 function MealDetails({duration, complexity, affordability}){
     return(
-        <View style={style.details}>
+        <View style={styles.details}>
             <Text style={styles.detailItem}>{duration}</Text>
             <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
             <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
